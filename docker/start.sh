1f206bb1be56c36db171237a5e7fd49a2c7b8e5b
Vic Garcia <vic.garcia@outlook.com>
2021-07-18 14:33:51 -0400

    bring things up to date

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 su docker <<EOF
     cd ~/code
-    echo "Build Project"
+    echo "install build tools"
+    npm install --only=dev
+    echo "build project"
     grunt build
-    echo "Setup development database"
+    echo "setup development database"
     php initdb.php
     echo "Start development server"
     php -S 0.0.0.0:8080 -t public
